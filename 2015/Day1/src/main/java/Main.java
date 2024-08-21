4465b9c1c88a28312ddf2b891545f9a4a3dfb7b5
Alfredo Oviedo <alfredooviedo@Alfredos-MBP.lan>
2024-08-20 22:29:54 -0500

    added input file

--- 2015/Day1/src/main/java/Main.java
+++ 2015/Day1/src/main/java/Main.java
@@ -1,7 +1,23 @@
-
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class Main {
   public static void main(String[] args) {
    System.out.println("Advent of Code 2015 - Day 1"); 
+   try {
+     
+   BufferedReader reader = new BufferedReader(new FileReader("input.txt"));
+   String line;
+   while ((line = reader.readLine()) != null) {
+
+     
+     System.out.println(line);
+     
+   }
+   reader.close();
+   } catch (IOException e) {
+     e.printStackTrace();
+   }
   }
 }
