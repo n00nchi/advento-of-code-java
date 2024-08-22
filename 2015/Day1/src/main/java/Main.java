d62a437c02f4f61ecbe46d6bdd6406aeaa710fe4
Alfredo Oviedo <alfredooviedo@Alfredos-MBP.lan>
2024-08-22 08:59:09 -0500

    part 1 passed

--- 2015/Day1/src/main/java/Main.java
+++ 2015/Day1/src/main/java/Main.java
@@ -7,14 +7,20 @@ public class Main {
    System.out.println("Advent of Code 2015 - Day 1"); 
    try {
      
-   BufferedReader reader = new BufferedReader(new FileReader("input.txt"));
+   BufferedReader reader = new BufferedReader(new FileReader("/Users/alfredooviedo/Projects/advent/advento-of-code-java/2015/Day1/src/main/java/input.txt"));
    String line;
+   int floor = 0;
    while ((line = reader.readLine()) != null) {
-
-     
-     System.out.println(line);
-     
+    for (int i = 0; i < line.length(); i++) {
+      char c = line.charAt(i);
+      if (c == '(') {
+        floor++;
+      } else if (c == ')') {
+        floor--;
+      }
+    }
    }
+   System.out.println("Final floor: " + floor);
    reader.close();
    } catch (IOException e) {
      e.printStackTrace();
